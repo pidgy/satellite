ac5cc4fff1728efe5bbf085284fe1d785bc8bcad
trashbo4t <trashbo4t@protonmail.com>
2018-06-03 12:46:23 -0400

    wrong err check

--- exampleclient/exampleclient.go
+++ exampleclient/exampleclient.go
@@ -19,19 +19,19 @@ func main() {
 	if err != nil {
 		log.Error().Err(err).Msg("Error sending Json")
 		return
-		log.Info().Msg("Object sent to the server bytes without error")
-		data := make([]byte, 1024)
-		n, err := conn.Read(data)
-		if err != nil {
-			log.Error().Err(err).Msg("Error receiving Json")
-			return
-		}
-		log.Info().Msgf("Received %d bytes from satellite", n)
-		msg, err := satellite.AsJson(data)
-		if err != nil {
-			log.Error().Err(err)
-			return
-		}
-		log.Info().Msgf("Received message: %s", msg)
 	}
+	log.Info().Msg("Object sent to the server bytes without error")
+	data := make([]byte, 1024)
+	n, err := conn.Read(data)
+	if err != nil {
+		log.Error().Err(err).Msg("Error receiving Json")
+		return
+	}
+	log.Info().Msgf("Received %d bytes from satellite", n)
+	msg, err := satellite.AsJson(data)
+	if err != nil {
+		log.Error().Err(err)
+		return
+	}
+	log.Info().Msgf("Received message: %s", msg)
 }
