c92510c524125d0f9903603260d8cffafc80e223
trashbo4t <trashbo4t@protonmail.com>
2018-06-03 12:38:54 -0400

    adding common lib and objects

---
+++ object.go
@@ -0,0 +1,17 @@
+package satellite
+
+/*
+	Your object to be marshaled
+ 	as JSON from raw bytes.
+
+ 	Add fields as you need this
+ 	object is shared between the
+ 	client and server.
+
+	This struct is parsed and modified in common.go
+*/
+type MyObject struct {
+	Magic uint8
+	Msg   string
+	Response string
+}
